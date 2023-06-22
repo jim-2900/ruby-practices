393d43cb2811e79c11bae7f440291d0cd4abb509
jim-2900 <caltai35@yahoo.co.jp>
2023-06-22 11:48:07 +0900

    指摘箇所を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,104 +5,74 @@ require 'date'
 
 params = ARGV.getopts("y:m:")
 
-
 y = params["y"].to_i
 m = params["m"].to_i
 
+year_now = Date.today.year
+month_now = Date.today.month
+
 if params["y"] == nil && params["m"] == nil
-  y = Date.today.year
-  m = Date.today.month
+  y = year_now
+  m = month_now
 elsif params["y"] == nil
-  y = Date.today.year
+  y = year_now
 elsif params["m"] == nil
-  m = Date.today.month
+  m = month_now
 end
 
-a = 0
-d = 2
-date1 = Date.new(y, m, 1)
-wday1 = date1.wday
+first_day = Date.new(y, m, 1)
+day_of_week_first_day = first_day.wday
 date_today = Date.today
 date_last = Date.new(y, m, -1)
 
 puts "      #{m}月 #{y}   "
 puts "日 月 火 水 木 金 土"
-date1 = Date.new(y, m, 1)
-wday1 = date1.wday
 
 # day 1 
-while a < 7 
 
-  if wday1 == a && a == 0
+(0..6).each do |day_of_week_number|
+  if  day_of_week_first_day == day_of_week_number && day_of_week_number == 0
     print " "
     print 1
-  elsif wday1 == a && wday1 == 6
+  elsif day_of_week_first_day == day_of_week_number && day_of_week_number == 6
     puts 1
-  elsif wday1 == a 
+  elsif day_of_week_first_day == day_of_week_number
     print 1
-  elsif wday1 >= a && a == 0
+  elsif day_of_week_first_day >= day_of_week_number && day_of_week_number == 0
     print "    "
-  elsif wday1 >= a 
+  elsif day_of_week_first_day >= day_of_week_number
     print "   "
   end
-
-  a += 1 
 end
 
 # day 2~
-while d < 32 do
+(2..31).each do |day|
 
-date = Date.new(y, m, d)
+  date = Date.new(y, m, day)
 
-today_color = "\e[30m\e[47m#{d}"
-color_reset = "\e[39m\e[49m"
-
-  if  date.saturday? && date == date_today && d > 9
-    puts " #{today_color}"
-    print color_reset
-  
-  elsif date.saturday? && d > 9  
-    puts " #{d}"
-  
-  elsif date.sunday? && date == date_today && d > 9
-    print today_color
-    print color_reset
+  if date.saturday? && day > 9  
+    puts " #{day}"
  
-  elsif date.sunday? && d > 9 
-    print d 
+  elsif date.sunday? && day > 9 
+    print day 
   
-  elsif date == date_today && d > 9 
-    print " #{today_color}"
-    print color_reset
-  
-  elsif  d > 9
-    print " #{d}"
+  elsif  day > 9
+    print " #{day}"
 
     # d < 10
-  elsif date.saturday? && date == date_today 
-    puts "  #{today_color}"
-    print color_reset
-  
   elsif date.saturday? 
-    puts "  #{d}"
-  
-  elsif date.sunday? && date == date_today
-    print " #{today_color}"
-    print color_reset
+    puts "  #{day}"
   
   elsif date.sunday?
-    print " #{d}"
-    print  
+    print " #{day}"
   
   else 
-    print "  #{d}"
+    print "  #{day}"
   
   end
 
-  if date ==  date_last
+  if date == date_last
     return
   end
-
-  d += 1
 end
 

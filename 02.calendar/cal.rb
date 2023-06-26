ffe62c5d0eb94f8b8d7aba6ea808546f03be398e
jim-2900 <caltai35@yahoo.co.jp>
2023-06-26 16:54:06 +0900

    動画レビュー視聴後の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,74 +5,47 @@ require 'date'
 
 params = ARGV.getopts("y:m:")
 
-y = params["y"].to_i
-m = params["m"].to_i
+year = params["y"].to_i
+month = params["m"].to_i
 
-year_now = Date.today.year
-month_now = Date.today.month
+current_year = Date.today.year
+current_month = Date.today.month
 
-if params["y"] == nil && params["m"] == nil
-  y = year_now
-  m = month_now
-elsif params["y"] == nil
-  y = year_now
-elsif params["m"] == nil
-  m = month_now
+if params["y"].nil?
+  year = current_year
 end
 
-first_day = Date.new(y, m, 1)
-day_of_week_first_day = first_day.wday
-date_today = Date.today
-date_last = Date.new(y, m, -1)
-
-puts "      #{m}月 #{y}   "
-puts "日 月 火 水 木 金 土"
-
-# day 1 
-
-(0..6).each do |day_of_week_number|
-  if  day_of_week_first_day == day_of_week_number && day_of_week_number == 0
-    print " "
-    print 1
-  elsif day_of_week_first_day == day_of_week_number && day_of_week_number == 6
-    puts 1
-  elsif day_of_week_first_day == day_of_week_number
-    print 1
-  elsif day_of_week_first_day >= day_of_week_number && day_of_week_number == 0
-    print "    "
-  elsif day_of_week_first_day >= day_of_week_number
-    print "   "
-  end
+if params["m"].nil?
+  month = current_month
 end
 
-# day 2~
-(2..31).each do |day|
+first_day = Date.new(year, month, 1)
+first_wday = first_day.wday
+last_day = Date.new(year, month, -1)
 
-  date = Date.new(y, m, day)
+puts "      #{month}月 #{year}"
+puts "日 月 火 水 木 金 土" 
 
-  if date.saturday? && day > 9  
-    puts " #{day}"
- 
-  elsif date.sunday? && day > 9 
-    print day 
-  
-  elsif  day > 9
-    print " #{day}"
+space = "   "
+print " #{space * first_wday}1"
 
-    # d < 10
-  elsif date.saturday? 
-    puts "  #{day}"
-  
-  elsif date.sunday?
-    print " #{day}"
-  
-  else 
-    print "  #{day}"
-  
+if first_wday == 6
+  print "\n"
+end
+
+(2..last_day.day).each do |day|
+
+  date = Date.new(year, month, day)
+
+  if date.sunday?
+    print "#{day}".rjust(2)
+  elsif
+    print "#{day}".rjust(3)
   end
 
-  if date == date_last
-    return
+  if date.saturday?
+    print "\n"
   end
+
 end
 

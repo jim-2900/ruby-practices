554e7e596043514f87c5d598259bd34cb64ce92f
jim-2900 <caltai35@yahoo.co.jp>
2023-06-28 17:10:46 +0900

    動画視聴後3度目の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -16,29 +16,22 @@ if params["m"].nil?
   month = Date.today.month
 end
 
+first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
 puts "      #{month}月 #{year}"
 puts "日 月 火 水 木 金 土" 
 
+space = "   "
+print "#{space * first_date.wday}"
+
 (1..last_date.day).each do |day|
   date = Date.new(year, month, day)
 
-  if day == 1
-    space = "   "
-    print " #{space * date.wday}1 "
+  print day.to_s.rjust(2)
+  print " "
   
-    if date.wday == 6
-      print "\n"
-    end
-  end
-
-  if day > 1
-    print day.to_s.rjust(2)
-    print " "
-  
-    if date.saturday?
-      print "\n"
-    end
+  if date.saturday?
+    print "\n"
   end
 end

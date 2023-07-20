411de6ea5c7d7f9ab391ded158905af8beeda9c3
jim-2900 <caltai35@yahoo.co.jp>
2023-07-20 18:17:25 +0900

    動画視聴後5度目の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,29 +8,19 @@ params = ARGV.getopts("y:m:")
 year = params["y"].to_i
 month = params["m"].to_i
 
-if params["y"].nil?
-  year = Date.today.year
-end
-
-if params["m"].nil?
-  month = Date.today.month
-end
+year = Date.today.year if params["y"].nil?
+month = Date.today.month if params["m"].nil?
 
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
 puts "      #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
-
 print "   " * first_date.wday
 
 (first_date..last_date).each do |date|
  
-
-  print date.day.to_s.rjust(2)
-  print " "
+  print "#{date.day.to_s.rjust(2)} "
+  print "\n" if date.saturday?
   
-  if date.saturday?
-    print "\n"
-  end
 end

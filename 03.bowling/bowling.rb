18c7549093e878d37ba85370ef60fdd6a2d623e0
jim-2900 <caltai35@yahoo.co.jp>
2023-12-19 15:59:43 +0900

    繰り返し処理をeach_with_index.sumに変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -15,19 +15,18 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = 0
-frames.each_with_index do |frame, i|
-  point += if i >= 9
-             frame.sum
-           elsif frame[0] == 10 && frames[i + 1][0] == 10 
-             20 + frames[i + 2][0]
-           elsif frame[0] == 10
-             10 + frames[i + 1].sum
-           elsif frame.sum == 10 
-             10 + frames[i + 1][0]
-           else
-             frame.sum
-           end
+point = frames.each_with_index.sum do |frame, i|
+  if i >= 9
+    frame.sum
+  elsif frame[0] == 10 && frames[i + 1][0] == 10
+    20 + frames[i + 2][0]
+  elsif frame[0] == 10
+    10 + frames[i + 1].sum
+  elsif frame.sum == 10
+    10 + frames[i + 1][0]
+  else
+    frame.sum
+  end
 end
 
 puts point
